980c00effd2005753e4444d4b582c37958323194
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2015-06-09 01:47:59 +0200

    Add maven project

---
+++ src/main/java/fr/xebia/xke/solid/App.java
@@ -0,0 +1,13 @@
+package fr.xebia.xke.solid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
